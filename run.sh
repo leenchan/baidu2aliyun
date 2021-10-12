ff0084a28b8321c7a4c546cf84836b7757c52213
leenchan <leenchan@gmail.com>
2021-10-12 15:42:40 +0800

    update

--- run.sh
+++ run.sh
@@ -1,2 +1,14 @@
 #!/bin/sh
+CUR_DIR=$(cd "$(dirname "$0")";pwd)
 
+install_required_packages() {
+  pip install aliyundrive-webdav
+  git clone https://github.com/qjfoidnh/BaiduPCS-Go.git baidupcs && \
+    cd baidupcs && \
+    go build && \
+    sudo mv BaiduPCS-Go /usr/bin/baidupcs && \
+    chmod +x /usr/bin/baidupcs
+  cd $CUR_DIR
+}
+
+install_required_packages
